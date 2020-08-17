0a596f36986ff9e981a3c7fcfbf62dfec57489cf
Frantz Fortunat <frantzfortunat@gmail.com>
2020-08-17 17:51:39 +0200

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :profiles, only: [ :new, :create, :show, :update ] do
+  resources :profiles, only: [ :index, :new, :create, :show, :edit :update ] do
   	resources :bookings, only: [ :new, :create ]
   end
   resources :dashboards do 
-  	resources :bookings, only: [ :show, :update ]
+  	resources :bookings, only: [ :index ]
   end
+  resources :bookings, only: [ :show, :edit, :update ]
 end
