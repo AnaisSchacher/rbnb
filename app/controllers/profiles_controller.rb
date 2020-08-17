05fea1384f361b8ddcede0edcde2831bd3e6f7d5
Segolene de la Taille <segolette@hotmail.com>
2020-08-17 17:55:27 +0200

    create profiles controller, actions new and create

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,24 @@
+class ProfilesController < ApplicationController
+
+  def new
+    @user = User.find(params[current_user.id])
+    @profile = Profile.new
+  end
+
+  def create
+    @user = User.find(params[current_user.id])
+    @profil = Profile.new(profil_params)
+    @profil.user = @user_profil
+      if @profil.save
+        redirect_to profil_path(@user_profil)
+      else
+        render :new
+      end
+  end
+
+
+  private
+  def profil_params
+    params.require(:profil).permit(:user_id, :address, :available, :category, :price, :description)
+  end
+end
