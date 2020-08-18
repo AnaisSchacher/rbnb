eba02b0bba7ad535c9e7c4e98062e786169af7e6
Anais Schacher <anais.schacher@gmail.com>
2020-08-18 14:32:34 +0200

    slides centrales'

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :profiles, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/dashboard", to: "users#dashboard"
 end
