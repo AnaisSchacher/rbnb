e0f05e24e5b46ae7e768cfeadfbad71c41827948
Frantz Fortunat <frantzfortunat@gmail.com>
2020-08-18 20:08:37 +0200

    list profiles + seed profiles et correction users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,45 @@
 require 'faker'
 require 'open-uri'
 
-puts 'Creating 50 fake users...'
-50.times do
+Profile.destroy_all
+User.destroy_all
+puts "Deleting all users..."
+
+puts 'Creating 6 fake users...'
+6.times do
   user = User.new(
-    username:    Faker::Name.last_name,
+    username: Faker::Name.last_name,
     name: Faker::Name.name,
     email: Faker::Internet.email,
     password: "123456",
   )
-    photo_id = 1
-    url = URI.open('https://source.unsplash.com/collection/people/250x250')
-    user.photo.attach(io: url, filename: "#{photo_id}.png")
+
+  puts "user_created"
+  file = URI.open('https://source.unsplash.com/collection/people/250x250')
+  puts "url_defined"
+  user.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+  puts "photo_attached"
+  # binding.pry
   user.save!
-  photo_id += 1
+  puts "user_saved"
 end
+
+puts "Deleting all profiles..."
+
+puts 'Creating 4 fake profiles...'
+4.times do
+  profile = Profile.new(
+    user: User.take,
+    address: Faker::Address.street_address,
+    available: true,
+    category: [ "Handicap mental", "Handicap sensoriel", "Handicap moteur", "Handicap psychique", "Maladies invalidantes" ].sample,
+    price: Faker::Number.between(from: 10, to: 30),
+    description: Faker::Lorem.paragraph(sentence_count: 4)
+  )
+  puts "profile_created"
+  profile.save!
+  puts "profile_saved"
+end
+
+
 puts 'Finished!'
