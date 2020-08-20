923ecdc814b7b697b4535bf41e8128a25c346141
Frantz Fortunat <frantzfortunat@gmail.com>
2020-08-20 17:39:47 +0200

    add name migration profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_091146) do
+ActiveRecord::Schema.define(version: 2020_08_20_150741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_091146) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "name"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
